85939b945508ac702b3963f43b10d2e3959271cc
tipex360 <tymon.suszczynski@gmail.com>
2023-07-31 15:59:04 +1000

    Renamed repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.DEPLOY_ENV === "github" ? '/next-portfolio' : '',
+    basePath: process.env.DEPLOY_ENV === "github" ? '/portfolio' : '',
     output: process.env.DEPLOY_ENV === "github" || process.env.DEPLOY_ENV === "local" ? 'export' : 'standalone',
     images: {
         unoptimized: process.env.DEPLOY_ENV === "github" ? true : undefined
