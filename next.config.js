c5d73c5fb32a025da3b0b581b37c771095274ffc
tipex360 <tymon.suszczynski@gmail.com>
2023-08-04 20:47:46 +1000

    Made DEPLOY_ENV NEXT_PUBLIC to enforce evaluation at build time

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.DEPLOY_ENV === "github" ? '/portfolio' : '',
-    output: process.env.DEPLOY_ENV === "github" || process.env.DEPLOY_ENV === "local" ? 'export' : 'standalone',
+    basePath: process.env.NEXT_PUBLIC_DEPLOY_ENV === "github" ? '/portfolio' : '',
+    output: process.env.NEXT_PUBLIC_DEPLOY_ENV === "github" || process.env.NEXT_PUBLIC_DEPLOY_ENV === "local" ? 'export' : 'standalone',
     images: {
-        unoptimized: process.env.DEPLOY_ENV === "github" ? true : undefined
+        unoptimized: process.env.NEXT_PUBLIC_DEPLOY_ENV === "github" ? true : undefined
     }
 }
 
