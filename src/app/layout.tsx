6ad4f6664575f9e72503651e940a3c037a15ee8a
tipex360 <tymon.suszczynski@gmail.com>
2023-10-10 20:06:40 +0930

    Google site verification[

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { fontNixieOne, fontMajorMonoDisplay } from "./utils/fonts"
-import FramerTransition from './utils/FramerTransition'
-import HomeIcon from './ui-components/HomeIcon'
+import "./globals.css";
+import type { Metadata } from "next";
+import { fontNixieOne, fontMajorMonoDisplay } from "./utils/fonts";
+import FramerTransition from "./utils/FramerTransition";
+import HomeIcon from "./ui-components/HomeIcon";
 
 export const metadata: Metadata = {
   title: "Tymoteusz Suszczynski",
-}
+  verification: {
+    google: "bcrjA3PMFyNzWz-tOWWN-lyPNiZBqvt51Htc4YJN-zs",
+  },
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html className={`${fontNixieOne.variable} ${fontMajorMonoDisplay.variable} scroll-smooth`} lang="en">
+    <html
+      className={`${fontNixieOne.variable} ${fontMajorMonoDisplay.variable} scroll-smooth`}
+      lang="en"
+    >
       <body className="font-body scroll-smooth overlow-x-clip">
-            <HomeIcon />
-            <FramerTransition>
-                {children}
-            </FramerTransition>
+        <HomeIcon />
+        <FramerTransition>{children}</FramerTransition>
       </body>
     </html>
-  )
+  );
 }
